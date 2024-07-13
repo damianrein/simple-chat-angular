19c81cfaa521f79b133c1c2dcc9ccd95646475a3
damianrein <damianrein464@gmail.com>
2024-07-12 23:30:10 -0300

    -

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChatComponent } from './components/chat/chat.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {path:'',component:LoginComponent},
-  {path : 'chat/:userId', component: ChatComponent}
+  {path:'register',component:RegisterComponent},
+  {path : 'chat/:userId', component: ChatComponent},
+  {path:'**',redirectTo:'',pathMatch:'full'}
 ];
 
 @NgModule({
